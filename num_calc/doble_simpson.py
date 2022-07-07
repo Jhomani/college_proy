e31c2ac1d6bb5fc87ec4c54c082fdfc5b6061185
J Carlos <jhomanidev@gmail.com>
2022-07-07 13:17:18 -0300

    on single file

--- num_calc/doble_simpson.py
+++ num_calc/doble_simpson.py
@@ -1,29 +1,29 @@
 import math
 from src.utils import my_round
 
-N = 3
-y_top = 4
+N = 150
+y_top = math.sqrt(3)
 y_bottom = 1
 
 def x_top(x: float, rounded = True):
-  res = 5
+  res = x
 
   return my_round(res,5) if rounded else res
 
 def x_bottom(x: float, rounded = True):
-  res = 2
+  res = 0
 
   return my_round(res,5) if rounded else res
 
 def input_fun(x: float, y: float, rounded = True):
-  res = (math.e)**(x-y)
+  res = x/(x**2+y**2)
 
   return my_round(res,5) if rounded else res
 
 #  Calculation from here
 hx = (y_top - y_bottom) / N
 
-print(f'hx = {my_round(hx)}')
+# print(f'hx = {my_round(hx)}')
 acumulated = 0
 
 def get_coof(position: int):
@@ -50,20 +50,20 @@ for i in range(N + 1):
   hy = (x_top(param_x) - x_bottom(param_x)) / N
   y_acumulated = 0
 
-  print(f'  hy = {hy}')
+  # print(f'  hy = {hy}')
   for j in range(N + 1):
     param_y = x_bottom(param_x) + (hy*j)
 
     res = input_fun(param_x,param_y) 
     y_acumulated += get_coof(j)*res
 
-    print(f'  G({my_round(param_x)},{my_round(param_y)}) = {res}*({get_coof(j)})')
+    # print(f'  G({my_round(param_x)},{my_round(param_y)}) = {res}*({get_coof(j)})')
 
   last_coe = (3/8) if (N % 3 == 0) else (1/3)
   partial_res = my_round((last_coe*hy)*y_acumulated)
   acumulated += get_coof(i)*partial_res
 
-  print(f'f(x{i}) = {partial_res}*({get_coof(i)})')
+  # print(f'f(x{i}) = {partial_res}*({get_coof(i)})')
 
 last_coe = (3/8) if (N % 3 == 0) else (1/3)
 final_res = (last_coe*hx)*acumulated
